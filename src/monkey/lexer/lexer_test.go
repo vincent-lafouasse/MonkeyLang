1eba2cb606fff025b126694ede6a8a245b6d3cc6
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-06-27 19:40:45 +0200

    implement tests

--- src/monkey/lexer/lexer_test.go
+++ src/monkey/lexer/lexer_test.go
@@ -23,4 +23,18 @@ func TestNextToken(t *testing.T) {
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
+
+	tokens := New(input)
+
+	for i, testCase := range tests {
+		token := tokens.NextToken()
+
+		if token.Type != testCase.expectedType {
+			t.Fatalf("tests[%d] - wrong token type, expected %q was %q", i, testCase.expectedType, token.Type)
+		}
+
+		if token.Literal != testCase.expectedLiteral {
+			t.Fatalf("tests[%d] - wrong literal, expected %q was %q", i, testCase.expectedLiteral, token.Literal)
+		}
+	}
 }
