7749ff951611d00b4a41fcc402471c93b1b6e3cb
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-06-27 19:23:26 +0200

    introduce identifiers

--- token/token.go
+++ token/token.go
@@ -6,3 +6,29 @@ type Token struct {
 	Type    TokenType
 	Literal string
 }
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// identifiers + literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
