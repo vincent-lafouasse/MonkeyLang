94cc7de827d60ee5fcd08b82a8f6e95cf8703711
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-06-27 19:55:43 +0200

    dummy implementation that fails tests

--- src/monkey/lexer/lexer.go
+++ src/monkey/lexer/lexer.go
@@ -1,23 +1,39 @@
 package lexer
 
+import (
+	"monkey/token"
+)
+
 type Lexer struct {
-	input string
-	position int
+	input        string
+	position     int
 	readPosition int
-	ch byte
+	ch           byte
 }
 
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
+	l.readChar()
 	return l
 }
 
 func (l *Lexer) readChar() {
-	if (l.readPosition >= len(l.input)) {
+	if l.readPosition >= len(l.input) {
 		l.ch = 0
 	} else {
 		l.ch = l.input[l.readPosition]
 	}
 	l.position = l.readPosition
 	l.readPosition++
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var output_token token.Token
+
+	output_token = newToken(token.PLUS, 0)
+	return output_token
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
